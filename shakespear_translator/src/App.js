51c57f00f37db3c197f2a25cd4981f47bd6e3866
ashleynicole472 <asha.4720@gmail.com>
2018-06-04 13:59:27 -0700

    added styling and images

--- shakespear_translator/src/App.js
+++ shakespear_translator/src/App.js
@@ -1,27 +1,31 @@
 import React, { Component } from 'react';
 import { navbar} from './bootstrap.min.css';
+import shakespeare from './images/shakespeare.jpg';
+import shakeOne from './images/shake-quote.png';
+import shakeTwo from './images/quote-shake.png';
 import './App.css';
 
 class App extends Component {
     constructor(props){
         super(props);
         this.state={
-            userText: ''
+            userText: '',
+            shakespeareText: ''
         }
         this.textareaHandleChange = this.textareaHandleChange.bind(this);
     }
 
     textareaHandleChange = (event) => {
-        console.log("in text area");
+        // console.log("in text area");
         this.setState({
             userText: event.target.value
         })
-        console.log(this.state.userText);
+        // console.log(this.state.userText);
     }
 
     speak = () => {
         let { userText } = this.state
-        let URL = `https://shakespeare.p.mashape.com/shakespeare.json?text=${user}`;
+        let URL = `https://shakespeare.p.mashape.com/shakespeare.json?text=${userText}`;
 
         var newHeaders = new Headers({
             "X-Mashape-Key": "Ma9KTYWObTmshpd7HvSsw6hea2dCp1DteDujsnmVQG8WQBwPo0"
@@ -34,7 +38,15 @@ class App extends Component {
 
         fetch(URL,config)
         .then((response) => {
-            console.log(response);
+            return(
+                response.json()
+            )
+        })
+        .then((data) => {
+            console.log(data);
+            this.setState({
+                shakespeareText: data.contents.translated
+            })
         })
         .catch((error) => {
             console.log(error);
@@ -62,16 +74,17 @@ class App extends Component {
                 <li className="nav-item">
                 </li>
               </ul>
-              <form className="form-inline my-2 my-lg-0">
-                <input className="form-control mr-sm-2" type="text" placeholder="Search" />
-                <button className="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-              </form>
             </div>
         </nav>
         <br />
         <div className="alert alert-dismissible alert-success">
           <button type="button" className="close" data-dismiss="alert" aria-label="close">&times;</button>
           <strong>Welcome!</strong> To get started, please enter any sentence or large chunk of modern English and we will translate it to Shakespeare.
+        </div>
+        <div className="col-md-12">
+            <img src={shakeOne} alt="photo of shakespeare" />
+            <img src={shakespeare} alt="photo of shakespeare" />
+            <img src={shakeTwo} alt="photo of shakespeare" />
         </div>
         <p>
             Convert from English to Shakespeare. Shakespeare invented many words and his style of
@@ -94,6 +107,10 @@ class App extends Component {
             type="button"
             onClick={this.speak}
             className="btn btn-primary btn-lg">Block level button</button>
+            <br />
+        <div className="well">
+            <h3 className="text-primary">{this.state.shakespeareText}</h3>
+        </div>
       </div>
     );
   }
